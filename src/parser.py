2401b3a648dcbec988bd933e98d7044fa8b91895
lladhibhutall <debjyoti.biswas@outlook.com>
2020-06-20 16:49:12 +0530

    Add program function

--- src/parser.py
+++ src/parser.py
@@ -6,6 +6,7 @@ TODO :
     can we do like a generator
 '''
 from .lexer import *
+import sys
 
 class Parser:
     def __init__(self, lexer):
@@ -15,15 +16,25 @@ class Parser:
         self.next_token()
         self.next_token()
     def abort(self, message):
-        pass
+        sys.exit(f"Parser Aborting Reason: {message}")
     #Returns True if current token matches
     def check_token(self, kind):
         return self.cur_token.kind == kind
     #Returns True if next token matches
     def check_peek(self, kind):
         return self.peek_token.kind == kind
+    #Gives the next token
     def next_token(self):
         self.cur_token = self.peek_token
         self.peek_token = self.lexer.get_token()
-    def match(self):
-        pass
+    #Tries to match return True if does and moves to next token else aborts
+    def match(self, kind):
+        if not self.check_token(kind):
+            self.abort(f"Expected {kind.name} got {self.cur_token.kind.name}")
+        self.next_token()
+        return True
+    def program(self):
+        while not self.match(TokenType.EOF):
+            self.statement() #Start from here; write the statement fucntion
+
+
